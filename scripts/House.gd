d9b4a13bf0725391e80ed14d598d758aafbc7338
Akbar Hashimi <snakelegendary@gmail.com>
2025-10-22 17:44:40 -0700

    created starter house code

---
+++ scripts/House.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+func _ready():
+	self.area_shape_entered.connect(add_score)
+	disable_house()
+	
+	add_to_group("House")
+	
+func add_score():
+	disable_house()
+	#send notification to player that they can update the score
+
+func disable_house():
+	visible = false
+	$CollisionShape2D.set_deferred("disabled",true)
+
+func enable_house():
+	visible = true
+	$CollisionShape2D.set_deferred("disabled",false)
