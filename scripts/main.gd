17b8e90f6dfe323fe72e6af487230953c0dd246f
rguidry2 <guidryriley@gmail.com>
2025-10-23 21:44:10 -0700

    Deleted comments

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,38 +1,5 @@
 extends Node
 
-# pseudo code
-
-'''
-// Level reads input
-
-// Get viable moves from Path2D? of tile player stands on
-	// player.location?
-	// array of tiles?
-	// get info from tile node?
-
-// Level moves player object to new tile
-	// direction interpreted from input
-	// add displacemet to player.location
-	// player.x += move_x
-	// player.y += move_y
-
-// Signal is emmitted from tile that was left
-	// house/gas/traffic/regular have different signals
-	// day counter/gas counter affected differently by player state
-	// Signal has info about tile that was left
-	
-// Level updates gas can & day counter
-	// gas can takes extra toll on traffic roads
-	// day get incremented more if player is driving through traffic
-
-// Car node only exists to track location if player is not in it
-	// Otheriwise hide/delete the object until player exits
-	// Gas can is handled by Level
-
-// Player node exists to manipulate sprites and keep track of location
-	// Movement/day counter is handled by Level
-'''
-
 var direction: String
 var move_x: int = 0
 var move_y: int = 0	
