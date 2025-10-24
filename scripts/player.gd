12662c4ffeba9dda3993dda4fe0b56e85746f547
nalhamal-CSUEB <nalhamal@horizon.csueastbay.edu>
2025-10-23 22:41:12 -0700

    changed order layer to make car visible

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,19 +2,26 @@ extends Area2D
 
 # custom methods
 var playerState = "car"
+@onready var car = $Car/CarSprite
+@onready var human = $Human/playerSprite
 
 
 #var b_isDriving: bool = false
 
 func becomeHuman():
-	$Human/playerSprite.visible = true
-	$Car/CarSprite.visible = true
+	human.visible = true
+	car.visible = true
+	car.z_index = 1
+	human.z_index = 2
+	
 	playerState = "human"
 	#b_isDriving = false
 
 func becomeCar():
-	$Human/playerSprite.visible = false
-	$Car/CarSprite.visible = true
+	human.visible = false
+	car.visible = true
+	car.z_index = 2
+	human.z_index = 1
 	playerState = "car"
 	#b_isDriving = true
 
