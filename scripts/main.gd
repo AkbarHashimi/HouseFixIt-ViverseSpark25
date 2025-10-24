255ac485e8284c4e3852d301ea8a4174d2ac614c
nalhamal-CSUEB <nalhamal@horizon.csueastbay.edu>
2025-10-23 19:10:38 -0700

    added level, and merged code

--- scripts/main.gd
+++ scripts/main.gd
@@ -39,101 +39,67 @@ var delay: int = 0
 var tile_size: int = 64
 
 @onready var player = $Player
-<<<<<<< Updated upstream
-@onready var level = $Level
-=======
 @onready var human = $Player/Human
 @onready var car = $Player/Car
-@onready var tile_map = $Level
-var astar_grid : AStarGrid2D
+@onready var level = $Level
 var carPosition : Vector2i
 var humanPosition : Vector2i
 var tilePosition : Vector2i
 var level_region : Vector2i
->>>>>>> Stashed changes
-
-
-<<<<<<< Updated upstream
-func _process(delta):
-	
-	if (delay > 0):
-		delay -= 1
-		return
-	
-	var direction: String
-	
-	# Convert action press into string
-	if (Input.is_action_pressed("ui_right")):
-		direction = "right"
-	elif (Input.is_action_pressed("ui_up")):
-		direction = "up"
-	elif (Input.is_action_pressed("ui_left")):
-		direction = "left"
-	elif (Input.is_action_pressed("ui_down")):
-		direction = "down"
-	
-	# Check move viability
-	var local_pos = level.to_local(player.global_position)
-	var map_pos = level.local_to_map(local_pos)
-	
-	var tile = level.get_cell_tile_data(map_pos)
-	var ruleset = tile.get_custom_data_by_layer_id(0)
-	
-	if direction not in ruleset:
-		return
-	
-	# Move player
-	var move_x: int = 0
-	var move_y: int = 0
-	
-	if (direction == "right"):
-		move_x = tile_size
-	elif (direction == "left"):
-		move_x = -tile_size
-	elif (direction == "up"):
-		move_y = -tile_size
-	elif (direction == "down"):
-		move_y = tile_size
-	
-	player.move_local_x(move_x)
-	player.move_local_y(move_y)
-	
-	delay = 15
-	
-	return
-=======
 
 func _ready():	
 	player.becomeCar()
 
-func _process(_delta):
-	pass
-
 func _input(_event):
+	var direction: String
 	var move_x: int = 0
 	var move_y: int = 0	
 	
+	# Convert action press into string
+	if (Input.is_action_pressed("ui_right")):
+		direction = "right"
+		move_x = tile_size
+	elif (Input.is_action_pressed("ui_up")):
+		direction = "up"
+		move_y = -tile_size
+	elif (Input.is_action_pressed("ui_left")):
+		direction = "left"
+		move_x = -tile_size
+	elif (Input.is_action_pressed("ui_down")):
+		direction = "down"
+		move_y =tile_size
+		
+	#Getting out into or out of car	
 	if (Input.is_key_pressed(KEY_X)):
 		if (player.playerState == "car"): 
 			player.becomeHuman()
-			print("Player is: ", player.playerState)
 		elif (player.playerState == "human"):
 			player.becomeCar()
-			print("Player is: ", player.playerState)
+	
+	# Check move viability
+	if (player.playerState == "car"):
+		var local_pos = level.to_local(player.global_position)
+		var map_pos = level.local_to_map(local_pos)
 		
-	if (Input.is_action_pressed("ui_right")):
-		move_x = 64
-	elif (Input.is_action_pressed("ui_left")):
-		move_x = -64
-	elif (Input.is_action_pressed("ui_up")):
-		move_y = -64
-	elif (Input.is_action_pressed("ui_down")):
-		move_y = 64
+		var tile = level.get_cell_tile_data(map_pos)
+		var ruleset = tile.get_custom_data_by_layer_id(0)
+	
+		if direction not in ruleset:
+			return	
+	else:
+		var local_pos = level.to_local(human.global_position)
+		var map_pos = level.local_to_map(local_pos)
+		
+		var tile = level.get_cell_tile_data(map_pos)
+		var ruleset = tile.get_custom_data_by_layer_id(0)
+	
+		if direction not in ruleset:
+			return	
+	
+	#Move player or human		
 	if(player.playerState == "car"):
 		player.move_local_x(move_x)
 		player.move_local_y(move_y)	
 	elif(player.playerState == "human"):
 		human.move_local_x(move_x)
 		human.move_local_y(move_y)
-	
->>>>>>> Stashed changes
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,14 +3,17 @@ extends Area2D
 # custom methods
 var playerState = "car"
 
+
 func becomeHuman():
 	$Human/playerSprite.z_index = 1
 	$Car/CarSprite.z_index = 0
 	$Human/playerSprite.visible = true
+	$Car/CarSprite.visible = true
 	playerState = "human"
 	
 func becomeCar():
 	$Human/playerSprite.z_index = 0
 	$Car/CarSprite.z_index = 1
 	$Human/playerSprite.visible = false
+	$Car/CarSprite.visible = true
 	playerState = "car"
