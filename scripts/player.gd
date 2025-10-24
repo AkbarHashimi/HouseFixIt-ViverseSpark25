edb19c3a29383d11cf8510dc735f204a4fbcda7a
rguidry2 <guidryriley@gmail.com>
2025-10-23 18:11:48 -0700

    Added isDriving boolean attribute to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,19 @@ extends Area2D
 
 # custom methods
 
+var b_isDriving: bool = false
+
 func becomeHuman():
 	$playerSprite.visible = true
 	$CarSprite.visible = false
 	
+	b_isDriving = false
+
 func becomeCar():
 	$playerSprite.visible = false
 	$CarSprite.visible = true
+	
+	b_isDriving = true
+
+func isDriving():
+	return b_isDriving
